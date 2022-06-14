26cfdddf49f8ada5f82655442bd05f4a486533cf
frankchiu12 <si-un_chiu@brown.edu>
2022-06-14 18:58:35 -0400

    code finished

--- PaneOrganizer.java
+++ PaneOrganizer.java
@@ -3,6 +3,8 @@ package chess;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -17,6 +19,12 @@ public class PaneOrganizer {
         Game game = new Game(gamePane);
         VBox vBox = new VBox();
 
+        ImageView imageView = new ImageView();
+        Image image = new Image("chess/chessPiecePNG/chessBackground.png");
+        imageView.setImage(image);
+        imageView.setOpacity(0.3);
+
+        this.root.getChildren().add(imageView);
         this.root.setCenter(gamePane);
         this.root.setRight(vBox);
 
